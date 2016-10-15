9a4d0c26e881b129faad7496f4c299808486b1c6
Eric Yu-Wei Chen <ericyu-weichen@bloopepper.local>
2016-10-15 14:07:55 +0800

    Initial Commit

---
+++ SuperCool01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool01
+//
+//  Created by Eric Yu-Wei Chen on 2016/10/15.
+//  Copyright © 2016年 Eric Yu-Wei Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
